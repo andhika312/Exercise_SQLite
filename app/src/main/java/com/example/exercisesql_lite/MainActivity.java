ca4aa4c373f910951dd7bb81235b2d4539847aa1
Andhika Putra Setianto <andhika312@gmail.com>
2020-04-16 23:37:01 +0700

    refresh main

--- app/src/main/java/com/example/exercisesql_lite/MainActivity.java
+++ app/src/main/java/com/example/exercisesql_lite/MainActivity.java
@@ -107,6 +107,8 @@ public class MainActivity extends AppCompatActivity {
                                                 }
                                                 newsList.clear();
                                                 reloadData();
+                                                finish();
+                                                startActivity(getIntent());
 
                                             }
                                         });
